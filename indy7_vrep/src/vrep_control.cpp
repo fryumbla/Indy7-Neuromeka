2165d22f499884917491b5cd041fe4d5f409e0d2
Francisco Yumbla <fryumbl@espol.edu.ec>
2020-08-11 20:53:21 +0900

    update

--- indy7_vrep/src/vrep_control.cpp
+++ indy7_vrep/src/vrep_control.cpp
@@ -5,7 +5,6 @@
 #include <std_msgs/Int32.h>
 #include <sensor_msgs/JointState.h>
 
-
 using namespace std;
 
 int clientID;
@@ -21,7 +20,6 @@ int found(int a,char* b,int c,int d)
     return c;
     cout << "joint " << d << " found"  << std::endl;
   }
-
 }
 
 void joint_callback(const sensor_msgs::JointState& data)
@@ -29,32 +27,22 @@ void joint_callback(const sensor_msgs::JointState& data)
   pub_msg.name=data.name;
   pub_msg.position = data.position;
 
-  char* joints[12]={"joint0","joint1","joint2","joint3","joint4","joint5","finger_joint", "left_inner_knuckle_joint","left_inner_finger_joint", "right_outer_knuckle_joint", "right_inner_knuckle_joint", "right_inner_finger_joint"};
-  // char* joints[12];
+  std::vector<char*> joints;
+  for (int i=0;i<data.name.size(); ++i){
+    char *cstr = new char[data.name.at(i).length() + 1];
+    strcpy(cstr, data.name.at(i).c_str());
+    joints.push_back(cstr);
+  }
 
-  // for (int i = 0; i < pub_msg.name.size(); i++)
-  // {
-  //   string name=pub_msg.name.at(i);
-  //   std::cout << name << '\n';
-  //   char* cstr= new char[pub_msg.name.size()];
-  //   name.copy(cstr, name.size());
-  //   // cstr[name.size()] = '\0';
-  // 	// strcpy(cstr,name.c_str());
-  //   joints[i]=cstr;
-  //   std::cout << cstr << '\n';
-  // }
-
-  // std::cout << joints[0] << '\n';
-  // std::cout << joints[1] << '\n';
-  // std::cout << joints[2] << '\n';
-  
-
-  int joint_handle[12]={0,0,0,0,0,0,0,0,0,0,0,0};
-
-  for (int i=0;i<=11; ++i){
+  std::vector<int> joint_handle;
+  for (int i = 0; i < data.name.size(); i++)
+  {
+    joint_handle.push_back(0);
+  }
+  for (int i=0;i<data.name.size(); ++i){
     joint_handle[i]=found(clientID,joints[i],joint_handle[i],i+1);
   }
-  for (int i=0;i<=11; ++i){
+  for (int i=0;i<data.name.size(); ++i){
     simxSetJointTargetPosition(clientID, (simxInt) joint_handle[i], data.position.at(i), simx_opmode_oneshot);
   }
 
@@ -69,12 +57,12 @@ int main(int argc, char **argv)
   
   if (clientID!=-1)
   {
-    cout << "Servidor conectado!" << std::endl;
+    cout << "Server conected!" << std::endl;
     
     ros::init(argc, argv, "vrep_communication");
     ros::NodeHandle nh = ros::NodeHandle();
     
-    ros::Subscriber sub = nh.subscribe("/joint_states", 2000, joint_callback);
+    ros::Subscriber sub = nh.subscribe("/joint_states", 5, joint_callback);
 
     // Waits for simulation time update.
     ros::Time last_ros_time_;
@@ -82,10 +70,10 @@ int main(int argc, char **argv)
 
     ros::spin();
     simxFinish(clientID); // fechando conexao com o servidor
-    cout << "Conexao fechada!" << std::endl;
+    cout << "Connection over!" << std::endl;
   }
   else
-    cout << "Problemas para conectar con servidor!" << std::endl;
+    cout << "Server connector problem!" << std::endl;
   return 0;
 }
 
